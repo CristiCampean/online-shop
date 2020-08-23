209429a8da755f3240d16f1b058b635aa38d48bd
”Cristi <cristi.campean@hotmail.com>
2020-08-23 08:28:30 +0300

    added user entity and datasource configuration

---
+++ src/main/java/org/fasttrackit/onlineshop/domain/User.java
@@ -0,0 +1,50 @@
+package org.fasttrackit.onlineshop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    @NotNull
+    private String fristName;
+    @NotNull
+    private String lastName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFristName() {
+        return fristName;
+    }
+
+    public void setFristName(String fristName) {
+        this.fristName = fristName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fristName='" + fristName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
