5f4939170bcaa8fa73ce658b5c424a01c2ecee16
”Cristi <cristi.campean@hotmail.com>
2020-08-31 07:46:07 +0300

    added create user method in user service

--- src/main/java/org/fasttrackit/onlineshop/service/UserService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/UserService.java
@@ -1,12 +1,15 @@
 package org.fasttrackit.onlineshop.service;
 
 import org.fasttrackit.onlineshop.domain.User;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistence.UserRepository;
 import org.fasttrackit.onlineshop.transfer.SaveUserRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -26,5 +29,13 @@ public class UserService {
     user.setFristName(request.getFirstName());
     user.setLastName(request.getLastName());
    return userRepository.save(user);
+    }
+
+    public  User getUser(long id){
+    LOGGER.info("Retrieving user {}", id);
+
+        Optional<User> userOptional = userRepository.findById(id);
+        return userRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("User"+ id + "does not exist"));
     }
 }
